8eda216c576008cf87cdea013cf177783f3de88a
jintheevil <eugenelimjq1217@gmail.com>
2022-03-29 16:06:16 +0800

    again

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(function(req, res, next) {
 });
  
 app.use(express.static('www'));
-app.set('port', process.env.PORT || 3000);
-app.listen(process.env.PORT || 3000, function(){
+app.set('port', process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });;
