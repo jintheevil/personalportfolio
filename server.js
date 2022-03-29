4163abe0e65eb037ea59c4a51b95048c2a23acfe
jintheevil <eugenelimjq1217@gmail.com>
2022-03-29 18:42:10 +0800

    again

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express  = require('express');
 var app      = express();                               
 var morgan = require('morgan');            
@@ -19,37 +18,8 @@ app.use(function(req, res, next) {
  
 app.use(express.static('www'));
 app.set('port', process.env.PORT || 5000);
-<<<<<<< HEAD
 app.listen(process.env.PORT || 5000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });;
-=======
-app.listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
-});
-=======
-var express  = require('express');
-var app      = express();                               
-var morgan = require('morgan');            
-var bodyParser = require('body-parser');    
-var cors = require('cors');
- 
-app.use(morgan('dev'));                                        
-app.use(bodyParser.urlencoded({'extended':'true'}));            
-app.use(bodyParser.json());                                     
-app.use(cors());
- 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
- 
-app.use(express.static('www'));
-app.set('port', process.env.PORT || 5000);
-app.listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
-});
->>>>>>> 226e8f7830b003e62f2f8386ae554844e3edfafc
->>>>>>> 982756074ed7f4de6e2643f6f2d88475bf3e09e2
+
+
